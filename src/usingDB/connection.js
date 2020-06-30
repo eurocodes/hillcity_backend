27d6f12dabe445b85280dc15908e33960c8a1f11
eurocodes <emmanuelumeoke@gmail.com>
2020-06-30 09:17:42 +0100

    modified connection again & again

--- src/usingDB/connection.js
+++ src/usingDB/connection.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const database = mysql.createConnection({
+const database = mysql.createPool({
     host: "us-cdbr-east-02.cleardb.com",
     user: "bac60cfb94e5ec",
     password: "dd5ba189",
@@ -11,17 +11,14 @@ const database = mysql.createConnection({
     multipleStatements: true,
 })
 
-database.connect(err => {
+database.getConnection((err, connection) => {
     if (!err) {
+        connection.release()
         console.log("Establshed connection with database")
     } else {
         console.log("Failed to connect to database", err.message)
     }
 })
 
-database.query("SELECT 1 + 1 AS solution", (error, results, fields) => {
-    if (error) throw error;
-    console.log("The solution is:", results[0].solution);
-})
 
 module.exports = database;
